1db84c87a52d5b63b26dc9688766150314f96616
Felipe Correa <pecolps@outlook.com>
2021-05-07 01:46:25 -0300

    wwwwww

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -153,6 +153,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_ROOT = 'media/'
+STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
+MEDIA_ROOT = '/media/'
 MEDIA_URL = '/media/'
